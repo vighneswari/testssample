de0d065683b0831ca9b23b0422e6e20695e49315
vighneswari <vighneswari.rao@a-cti.com>
2017-04-06 21:35:54 +0530

    testing commit2

---
+++ src/com/testgit2/Testgit2Servlet.java
@@ -0,0 +1,12 @@
+package com.testgit2;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Testgit2Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
